dcf6d38430c23275c5af5abe40b2aa81c6aef860
Ole Jensen <ole_j@hotmail.com>
2017-03-27 23:33:19 +0200

    Tutorial done!

--- PictureViewer/Form1.Designer.cs
+++ PictureViewer/Form1.Designer.cs
@@ -141,7 +141,6 @@
             // 
             // openFileDialog1
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
             this.openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" +
     "s (*.*)|*.* ";
             this.openFileDialog1.Title = "Select a picture file";
